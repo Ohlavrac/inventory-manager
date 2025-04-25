899a38aaa7a0e92e364201a8cbb48d54560b79d0
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-04-25 12:33:37 -0300

    feat: add category entity

--- src/main/java/com/ohlavrac/inventory_manager/domain/entities/CategoryEntity.java
+++ src/main/java/com/ohlavrac/inventory_manager/domain/entities/CategoryEntity.java
@@ -1,18 +1,31 @@
 package com.ohlavrac.inventory_manager.domain.entities;
 
+import java.util.List;
+import java.util.UUID;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Table(name = "categories")
+@Table(name = "categorie")
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CategoryEntity {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
     private String categoryName;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<ProductEntity> products;
 }
