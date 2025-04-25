f0ef7f8f271480e81e97c6dd8d84db211c38758e
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-04-25 12:33:45 -0300

    feat: add order entity

--- src/main/java/com/ohlavrac/inventory_manager/domain/entities/OrderEntity.java
+++ src/main/java/com/ohlavrac/inventory_manager/domain/entities/OrderEntity.java
@@ -1,5 +1,27 @@
 package com.ohlavrac.inventory_manager.domain.entities;
 
+import java.util.UUID;
+
+import org.hibernate.annotations.SecondaryRow;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "order")
+@Entity
+@Getter
+@SecondaryRow
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderEntity {
-    
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String orderName;
 }
