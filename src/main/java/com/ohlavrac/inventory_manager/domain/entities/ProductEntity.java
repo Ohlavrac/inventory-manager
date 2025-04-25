5393fd142fe27437cc9f0e08c9c056e5ea33ea39
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-04-25 12:33:28 -0300

    feat: add product entity

--- src/main/java/com/ohlavrac/inventory_manager/domain/entities/ProductEntity.java
+++ src/main/java/com/ohlavrac/inventory_manager/domain/entities/ProductEntity.java
@@ -1,18 +1,41 @@
 package com.ohlavrac.inventory_manager.domain.entities;
 
+import java.util.UUID;
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Table(name = "products")
+@Table(name = "product")
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductEntity {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
     private String productName;
+    private String brand;
+    private int amount;
+    private double value;
+
+    @ManyToMany
+    @JoinTable(
+        name = "product_categories",
+        joinColumns = @JoinColumn(name = "product_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private List<CategoryEntity> categories;
 }
