73ca99e86ad8bbbd1fa97fe6e5283bc4d87ecf28
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-04-28 10:15:08 -0300

    feat: add product repository

---
+++ src/main/java/com/ohlavrac/inventory_manager/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.ohlavrac.inventory_manager.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ohlavrac.inventory_manager.domain.entities.ProductEntity;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
+    
+}
