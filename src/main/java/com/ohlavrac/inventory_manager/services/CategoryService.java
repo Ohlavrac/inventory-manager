1bb81ba36c8f6e3ab3e96f16dddbadb9cedd8c79
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-04-28 12:08:17 -0300

    feat: add category create

---
+++ src/main/java/com/ohlavrac/inventory_manager/services/CategoryService.java
@@ -0,0 +1,30 @@
+package com.ohlavrac.inventory_manager.services;
+
+import org.springframework.stereotype.Service;
+
+import com.ohlavrac.inventory_manager.domain.entities.CategoryEntity;
+import com.ohlavrac.inventory_manager.dtos.category.CategoryRequestDTO;
+import com.ohlavrac.inventory_manager.dtos.category.CategoryResponseDTO;
+import com.ohlavrac.inventory_manager.mappers.CategoryMapper;
+import com.ohlavrac.inventory_manager.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryService(
+        CategoryRepository categoryRepository,
+        CategoryMapper categoryMapper
+    ) {
+        this.categoryRepository = categoryRepository;
+        this.categoryMapper = categoryMapper;
+    }
+
+    public CategoryResponseDTO createCategory(CategoryRequestDTO categoryData) {
+        CategoryEntity categoryEntity = categoryMapper.requestDtoToEntity(categoryData);
+        CategoryEntity newCategory = categoryRepository.save(categoryEntity);
+
+        return categoryMapper.entityToResponseDto(newCategory);
+    }
+}
