157857853ac4ff4848eb74b6932f45ac22854705
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-04-28 08:55:24 -0300

    feat: add product in order

--- src/main/java/com/ohlavrac/inventory_manager/domain/entities/OrderEntity.java
+++ src/main/java/com/ohlavrac/inventory_manager/domain/entities/OrderEntity.java
@@ -7,6 +7,8 @@ import org.hibernate.annotations.SecondaryRow;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,4 +26,9 @@ public class OrderEntity {
     private UUID id;
 
     private String orderName;
+    private int quantOrder;
+    
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private ProductEntity productOrder;
 }
