71218495fcf5d5c23172bbaf5c9d9783b424b9e6
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-05-02 11:56:10 -0300

    feat: add category controller

---
+++ src/main/java/com/ohlavrac/inventory_manager/controllers/CategoryController.java
@@ -0,0 +1,28 @@
+package com.ohlavrac.inventory_manager.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ohlavrac.inventory_manager.dtos.category.CategoryRequestDTO;
+import com.ohlavrac.inventory_manager.dtos.category.CategoryResponseDTO;
+import com.ohlavrac.inventory_manager.services.CategoryService;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+    
+    @PostMapping()
+    public ResponseEntity<CategoryResponseDTO> createCategory(@RequestBody CategoryRequestDTO categoryData) {
+        CategoryResponseDTO result = categoryService.createCategory(categoryData);
+        return ResponseEntity.ok().body(result);
+    }
+    
+}
