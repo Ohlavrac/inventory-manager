ef878a0847aa8ec692d5656da57c54b2a3c40dea
ohlavrac <joaovicrcarvalho@gmail.com>
2025-05-23 12:02:24 -0300

    fix: order table name in order entity

--- src/main/java/com/ohlavrac/inventory_manager/domain/entities/OrderEntity.java
+++ src/main/java/com/ohlavrac/inventory_manager/domain/entities/OrderEntity.java
@@ -1,9 +1,13 @@
 package com.ohlavrac.inventory_manager.domain.entities;
 
+import java.util.Date;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SecondaryRow;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -13,9 +17,11 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Table(name = "order")
+@Table(name = "orders")
 @Entity
+@Setter
 @Getter
 @SecondaryRow
 @AllArgsConstructor
@@ -25,10 +31,22 @@ public class OrderEntity {
     @GeneratedValue
     private UUID id;
 
+    @Column(name = "order_name")
     private String orderName;
+
+    @Column(name = "quant_order")
     private int quantOrder;
+
+    @Column(name = "order_description")
+    private String description;
     
     @ManyToOne
     @JoinColumn(name = "product_id", referencedColumnName = "id")
     private ProductEntity productOrder;
+
+    @CreationTimestamp
+    private Date created_at;
+
+    @UpdateTimestamp
+    private Date updated_at;
 }
