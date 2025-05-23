ff22c568a5bee9a0b37d69ca86f9245e84bbdc4d
ohlavrac <joaovicrcarvalho@gmail.com>
2025-05-23 11:45:47 -0300

    feat: add RersourceAmountException

---
+++ src/main/java/com/ohlavrac/inventory_manager/exceptions/ResourceAmountExecption.java
@@ -0,0 +1,7 @@
+package com.ohlavrac.inventory_manager.exceptions;
+
+public class ResourceAmountExecption extends RuntimeException {
+    public ResourceAmountExecption(String message) {
+        super(message);
+    }
+}
