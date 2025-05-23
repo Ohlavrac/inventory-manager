d4bc5a4ca43506d0b942e5002df49422db2669ce
ohlavrac <joaovicrcarvalho@gmail.com>
2025-05-23 09:41:41 -0300

    feat: add Delete Exception

---
+++ src/main/java/com/ohlavrac/inventory_manager/exceptions/DeleteException.java
@@ -0,0 +1,7 @@
+package com.ohlavrac.inventory_manager.exceptions;
+
+public class DeleteException extends RuntimeException {
+    public DeleteException(String message) {
+        super(message);
+    }
+}
