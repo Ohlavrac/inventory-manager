d2e803b1d73b1db4adb21e8d9a74179a8e8a1330
ohlavrac <joaovicrcarvalho@gmail.com>
2025-05-26 16:04:59 -0300

    feat: add custom entity response handler for exceptions

---
+++ src/main/java/com/ohlavrac/inventory_manager/exceptions/hadler/CustomEntityResponseHandler.java
@@ -0,0 +1,41 @@
+package com.ohlavrac.inventory_manager.exceptions.hadler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.ohlavrac.inventory_manager.exceptions.DeleteException;
+import com.ohlavrac.inventory_manager.exceptions.ExceptionResponse;
+
+@RestController
+@ControllerAdvice
+public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler {
+    
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception exception, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+            new Date(),
+            exception.getMessage(),
+            request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    
+    @ExceptionHandler(DeleteException.class)
+    public final ResponseEntity<ExceptionResponse> handleDeleteException(Exception exception, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+            new Date(),
+            exception.getMessage(),
+            request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+}
