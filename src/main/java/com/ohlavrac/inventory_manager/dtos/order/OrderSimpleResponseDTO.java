77c9c5027f9f6505067eb7aca88a858bcc4b8400
ohlavrac <joaovicrcarvalho@gmail.com>
2025-05-29 10:09:44 -0300

    feat: add order status in order simple responde dto

--- src/main/java/com/ohlavrac/inventory_manager/dtos/order/OrderSimpleResponseDTO.java
+++ src/main/java/com/ohlavrac/inventory_manager/dtos/order/OrderSimpleResponseDTO.java
@@ -2,12 +2,15 @@ package com.ohlavrac.inventory_manager.dtos.order;
 
 import java.util.UUID;
 
+import com.ohlavrac.inventory_manager.domain.enums.OrderStatus;
+
 public record OrderSimpleResponseDTO(
     UUID id,
     String orderName,
     int quantOrder,
     String description,
-    String productName
+    String productName,
+    OrderStatus orderStatus
 ) {
     
 }
