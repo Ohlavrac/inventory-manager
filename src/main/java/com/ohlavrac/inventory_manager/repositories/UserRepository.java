d2a8ca40b6381d4f7289851878ae179cc334ece9
ohlavrac <joaovicrcarvalho@gmail.com>
2025-06-10 09:47:38 -0300

    feat: add query to update user role

--- src/main/java/com/ohlavrac/inventory_manager/repositories/UserRepository.java
+++ src/main/java/com/ohlavrac/inventory_manager/repositories/UserRepository.java
@@ -4,12 +4,23 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ohlavrac.inventory_manager.domain.entities.user.UserEntity;
+import com.ohlavrac.inventory_manager.domain.enums.UserRoles;
+
+import jakarta.transaction.Transactional;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE UserEntity u SET u.userRole = :userRole WHERE u.id = :id")
+    void updateUserRole(@Param(value="userRole") UserRoles userRole, @Param(value="id") UUID id);
 }
