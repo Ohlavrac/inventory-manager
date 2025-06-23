d739c5e80584b8a411b837bc12bfaa3ffb37b259
ohlavrac <joaovicrcarvalho@gmail.com>
2025-06-23 11:29:23 -0300

    fix: use cleanAuto to clear update query cache

--- src/main/java/com/ohlavrac/inventory_manager/repositories/UserRepository.java
+++ src/main/java/com/ohlavrac/inventory_manager/repositories/UserRepository.java
@@ -19,7 +19,7 @@ import jakarta.transaction.Transactional;
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmail(String email);
 
-    @Modifying
+    @Modifying(clearAutomatically = true)
     @Transactional
     @Query("UPDATE UserEntity u SET u.userRole = :userRole WHERE u.id = :id")
     void updateUserRole(@Param(value="userRole") UserRoles userRole, @Param(value="id") UUID id);
