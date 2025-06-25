a6d286f3f890bd112964049e7c43c77783e2814f
ohlavrac <joaovicrcarvalho@gmail.com>
2025-06-25 10:12:02 -0300

    feat: add a user mapper toEntity (from a dto)

--- src/main/java/com/ohlavrac/inventory_manager/mappers/UserMapper.java
+++ src/main/java/com/ohlavrac/inventory_manager/mappers/UserMapper.java
@@ -1,6 +1,7 @@
 package com.ohlavrac.inventory_manager.mappers;
 
 import com.ohlavrac.inventory_manager.domain.entities.user.UserEntity;
+import com.ohlavrac.inventory_manager.dtos.users.CreateUserDTO;
 import com.ohlavrac.inventory_manager.dtos.users.UserResponseDTO;
 
 public class UserMapper {
@@ -11,5 +12,16 @@ public class UserMapper {
             entity.getEmail(), 
             entity.getUserRole()
         );
+    }
+
+    public UserEntity toEntity(CreateUserDTO dto) {
+        UserEntity entity = new UserEntity();
+
+        entity.setEmail(dto.email());
+        entity.setUserName(dto.username());
+        entity.setPassword(dto.password());
+        entity.setUserRole(dto.role());
+
+        return entity;
     }
 }
